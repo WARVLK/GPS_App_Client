245d64aab256bb9775b9c933e57b7a37bbadb077
Vladis <brezinavlado@gmail.com>
2020-07-11 13:21:05 +0200

    Authorization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
+import { HttpClient } from '@angular/common/http'
+import { ActivatedRoute } from '@angular/router';
+
+export const OAuthConfig: AuthConfig = {
+  issuer: 'https://www.strava.com/api/v3/oauth/authorize',
+  loginUrl: 'https://www.strava.com/api/v3/oauth/authorize',
+  oidc: false,
+  redirectUri: window.location.origin,
+  clientId: '50927',
+  responseType: 'code',
+  scope: 'activity:write,read',
+  tokenEndpoint: 'https://www.strava.com/api/v3/oauth/token',
+  customQueryParams: {approval_prompt:'auto'}
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+@Injectable()
+export class AppComponent implements OnInit{
+
+  constructor(private oAuthService: OAuthService, private activatedRoute: ActivatedRoute){}
+
+  Onclick(){
+    this.oAuthService.configure(OAuthConfig);
+    this.oAuthService.initImplicitFlow();
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      if(params['code']== null) return;
+      console.log(params); 
+    })
+  }
   title = 'Client';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    OAuthModule.forRoot(),
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
